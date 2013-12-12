501609a06f315aff0251964e37e306a234ddbda0
Alec Brownlie <brownlia@seawolf.cs.sonoma.edu>
2013-12-12 13:42:00 -0800

    Resubmitted dot_files on 12/12/13

--- setup.sh
+++ setup.sh
@@ -1,1 +1,61 @@
-#nothing
+echo "New folder name"
+read foldername
+echo "What should the file name be?"
+read filename
+
+
+echo "What type of project do you want to generate?\n
+1) .cpp
+2) .h
+Enter a number"
+read projecttype
+
+if [ $projecttype == 1 ]; then
+    mkdir $foldername
+    cd $foldername
+    touch $filename.cpp
+    mainfile=$filename.cpp
+    echo "
+//Author: Alec Brownlie
+
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+return 0
+}" >> $mainfile
+
+
+elif [ $projecttype == 2 ]; then
+    
+    mkdir $foldername
+    cd $foldername
+    touch $filename.h
+    mainfile=$filename.h
+    echo" 
+//Author: Alec Brownlie
+
+#include <iostream>
+
+using namespace std;
+template <class LT> class $foldername;
+
+template \<class LT\>
+class $filename
+{
+public:
+
+private:
+}
+
+" >> $mainfile
+fi 
+
+
+echo "File name: $mainfile
+Author: $(whoami)
+Date: $(date)" > README.md 
+
+echo DONE
